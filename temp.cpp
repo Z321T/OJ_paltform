ba762a7c784322c8feddf8376a16b4a63135c423
HEPUXI <107572072+HEPUXI@users.noreply.github.com>
2024-07-13 22:26:00 +0800

    coding

--- temp.cpp
+++ temp.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-
-int main() {
-    int num1, num2, sum;
-
-    std::cin >> num1;
-
-    std::cin >> num2;
-
-    sum = num1 + num2;
-
-    std::cout << sum ;
-
-    return 0;
-}
+cout << "Hello, world!" << endl;
