e23ed5d681b0f5f5bfa1eaca2b79d109cbc8230a
Z321T <fzc19942496944@outlook.com>
2024-07-14 16:10:55 +0800

    修改

--- login/BERT_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import (ProgrammingCodeFeature, ProgrammingReportFeature,
-                     ReportStandardScore)
-
-# Register your models here.
-# admin.site.register(CodeFeature)
-admin.site.register(ProgrammingCodeFeature)
-admin.site.register(ProgrammingReportFeature)
-admin.site.register(ReportStandardScore)
